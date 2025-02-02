3b3d75a1224808f59ca777f4939d538c5ffef89e
Nicolas Giacomelli <ni.giacomelli@gmail.com>
2025-02-02 16:40:36 -0300

    adicao de templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': "Nicolas"
+    })
+
+def contato(request):
+    return HttpResponse("CONTATO")
+
+def sobre(request):
+    return HttpResponse("SOBRE ")
